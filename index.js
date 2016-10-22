8bd5519c621a091f592f6b1eff77c90ee7cb0ac3
Alex Macleod <alex@macleod.io>
2016-10-22 21:51:10 +0100

    Insert and query database for urls

--- index.js
+++ index.js
@@ -17,19 +17,50 @@ const pool = new pg.Pool({
   ssl: true
 })
 
-app.get('/:url', function(request, response) {
-  console.log('url')
+const RADIX = 36
+
+// Ignore for debugging
+app.get('/favicon.ico', function(request, response) {
+  response.sendStatus(200)
 })
 
-app.get('/new/:url', function(request, response) {
-  console.log('new/url')
+app.get('/:short', function(request, response) {
+  const id = parseInt(request.params.short, RADIX)
+
+  pool.query(`SELECT url FROM urls WHERE id=$1;`, [id])
+    .then(result => {
+      response.redirect(result.rows[0].url)
+    })
+    .catch(console.error.bind(console))
+})
+
+app.get(/^\/new\/.+/, function(request, response) {
+  const newUrl = url.parse(request.url.slice(5))
+
+  response.setHeader('Content-Type', 'application/json')
+
+  if (!(newUrl.protocol && newUrl.host)) {
+    response.status(400)
+    response.end('{"error": "bad request"}')
+    return
+  }
+
+  pool.query(`INSERT INTO urls (url) VALUES ($1) RETURNING id;`, [newUrl.href])
+    .then(result => {
+      const id = result.rows[0].id
+      response.end(JSON.stringify({
+        original_url: newUrl.href,
+        short_url: id.toString(RADIX)
+      }))
+    })
+    .catch(console.error.bind(console))
 })
 
 pool
-  .query(`CREATE TABLE IF NOT EXISTS urls(
+  .query(`CREATE TABLE IF NOT EXISTS urls (
     id   SERIAL  PRIMARY KEY,
     url  TEXT    NOT NULL
   );`)
   .then(function() {
-    app.listen(prcoess.env.PORT || 8080)
+    app.listen(process.env.PORT || 8080)
   })
